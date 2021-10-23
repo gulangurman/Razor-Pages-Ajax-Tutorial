0a64ec781c6f6977b4a15d911c80ca525d53927e
Gulan Gurman <76517407+gulangurman@users.noreply.github.com>
2021-10-24 00:33:07 +0300

    Add project files.

---
+++ RazorPagesAjaxTutorial/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesAjaxTutorial.Pages
+{
+    /***
+     * How to use Ajax with Razor Pages
+     * 
+     * Author: Gülan Gürman
+     * Date: 24.10.2021
+     */
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
